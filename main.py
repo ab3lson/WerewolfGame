1ff5134c5df327b7a9c6b264e3b0a2cedcef0158
ab3lson <ablec@byu.edu>
2020-02-02 02:35:43 -0700

    added new pages

--- main.py
+++ main.py
@@ -8,6 +8,18 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("home.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
+
+@app.route("/game")
+def game():
+    return render_template("game.html")
     
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=3088,debug=True)
