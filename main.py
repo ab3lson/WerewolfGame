87bf6df054d3abaff5a2fb7772ea1aee960c0595
ab3lson <ablec@byu.edu>
2020-02-03 22:21:53 -0700

    added user session variables

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 #! /usr/bin/python3
 #bootstrap theme found at https://bootswatch.com/darkly/
 
-from flask import Flask, render_template
+from flask import Flask, render_template, session, request, redirect, send_from_directory
+import os
 
 app = Flask(__name__)
+app.secret_key = "WEareBYUstudents!"
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico')
 
 @app.route("/")
 def home():
@@ -13,13 +19,30 @@ def home():
 def login():
     return render_template("login.html")
 
+@app.route("/verify",methods=['POST'])
+def verify():
+#TODO: insert code to check database for valid login
+    session["username"] = request.form['username']
+    session['loggedIn'] = 1
+    return redirect("/game")
+
+@app.route("/signout")
+def signout():
+    session.clear()
+    return redirect("/")
+
 @app.route("/signup")
 def signup():
     return render_template("signup.html")
 
 @app.route("/game")
 def game():
-    return render_template("game.html")
+    try:
+        if session["loggedIn"]:
+            return render_template("game.html")
+    except:
+        pass
+    return redirect("/login")
     
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=3088,debug=True)
