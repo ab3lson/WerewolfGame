8f461940b172fd65e5dda0db09378a692cf30438
ab3lson <ablec@byu.edu>
2020-02-09 23:58:03 -0700

    added lobby support and user inc and dec in DB

--- main.py
+++ main.py
@@ -155,11 +155,15 @@ def createGame():
             except:
                 pass #the user is not already in a room
             decisionSeconds = int(request.form["decisionTimer"])*60
-            session["roomId"] = roomGenerator()
+            newRoomId = roomGenerator()
+            session["roomId"] = newRoomId
             print("*****************\nROOM ID GENERATED: {}".format(session["roomId"]))
             print("Variables from form:\nDecision in seconds:",decisionSeconds,"\nPlayers needed:", request.form["playersNeeded"])
             print("*****************")
-            ##TODO: get values and create lobby in database
+            playersInt = int(request.form["playersNeeded"])
+            cur = con.cursor()
+            cur.execute("INSERT INTO Lobby (RoomId, PlayersNeeded, DecisionTimer) VALUES (%s, %s, %s)",(newRoomId,int(request.form["playersNeeded"]),int(decisionSeconds)))
+            cur.close()
             newGame = {"roomId":session["roomId"],"players":[session["username"]],"playersNeeded":request.form["playersNeeded"],"decisionTimer":decisionSeconds}
             GAMES.append(newGame)
             print("Making new game:",newGame)
@@ -180,11 +184,13 @@ def joinGame():
         except:
             pass #the user is not already in a room
         if session["loggedIn"]:
-            #TODO: add if statement to see if roomId is in the database. if it is then join it, if not redirect and display error on play.html
             for game in GAMES:
                 if request.form["roomId"] == game["roomId"]:
                     session["roomId"] = request.form["roomId"]
                     game["players"].append(session["username"])
+                    cur = con.cursor()
+                    cur.execute("UPDATE Lobby SET CurrentPlayers = CurrentPlayers + 1 WHERE RoomId = %s",(session["roomId"]))
+                    cur.close()
                     print("****************")
                     print("The user {} is joining the lobby: {}\n***************".format(session["username"],session["roomId"]))
                     roomId = session["roomId"]                            
@@ -207,7 +213,7 @@ def lobby():
                     currentPlayers=len(game["players"])
                     playersNeeded=game["playersNeeded"]
                     gameDict = game
-            if currentPlayers == playersNeeded:
+            if currentPlayers == playersNeeded:#TODO: add all players from players list in GAMES to ActiveGames in the DB
                 pass #TODO: emit a message to all players in the lobby and redirect them to /pregame where roles get assigned
             else:
                 return render_template("lobby.html",roomId=session["roomId"],currentPlayers=currentPlayers,playersNeeded=playersNeeded,playerNames=gameDict["players"])
@@ -226,13 +232,17 @@ def leaveLobby():
                         if user == session["username"]:
                             game["players"].remove(user)
                             print("The user {} left the lobby: {}".format(session["username"],session["roomId"]))
-                            #TODO: remove user from active game and decrement player count in database
+                            cur = con.cursor()
+                            cur.execute("UPDATE Lobby SET CurrentPlayers = CurrentPlayers - 1 WHERE RoomId = %s",(session["roomId"]))
+                            cur.close()
                     print("Players left in the lobby:",len(game["players"]))
                     if len(game["players"]) == 0:
                         print("Nobody is in the lobby: {}. Deleting the lobby.".format(session["roomId"]))
+                        cur = con.cursor()
+                        cur.execute("DELETE FROM Lobby WHERE RoomId = %s",(session["roomId"]))
+                        cur.close()
                         GAMES.remove(game)
                         print("Remaining lobbies:",GAMES)
-                        #TODO: remove roomId lobby in database
             session.pop('roomId', None)
             print("***************")
             return redirect("/game")
@@ -295,8 +305,10 @@ def dbtest():
         print(x["Username"])
     cur.close()
     return redirect("/")
-
-
+@app.route("/sessionDestroy")
+def sessionDestroy():
+	session.clear()
+	return redirect("/")
 
 @app.errorhandler(404)
 def page_not_found(error):
